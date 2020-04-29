4683418d4ba8b436d6d0e66ad6c317c40e31650d
Zelong <zhaozelong1996@gmail.com>
2020-04-29 19:08:44 -0400

    Frontend login page

---
+++ client/build/precache-manifest.1f59e866c793d3be19ebc574fb94ce0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de88d67d6ee8b467d624bbd8bbf1bab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8db749cfaabe753cfa56",
+    "url": "/static/css/main.6cfff724.chunk.css"
+  },
+  {
+    "revision": "477f51b09f3df388c2c5",
+    "url": "/static/js/2.1899d857.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1899d857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db749cfaabe753cfa56",
+    "url": "/static/js/main.9095c7a0.chunk.js"
+  },
+  {
+    "revision": "1feb6cb50c926b92a852",
+    "url": "/static/js/runtime-main.2510f9ea.js"
+  }
+]);
