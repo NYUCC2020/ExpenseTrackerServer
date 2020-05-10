0225c4c7f78601e80f3a1425e0adad3dcfe61c04
William Ng <iamnwi@pku.edu.cn>
2020-05-10 01:41:04 -0400

    Revert "put client code in another repo"

---
+++ client/README.md
@@ -0,0 +1,8 @@
+## Front End (React)
+```
+npm install
+
+# Run on http://localhost:3000
+npm start
+```
+
---
+++ client/src/components/TransactionList.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from 'react';
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
+import { useSelector } from 'react-redux';
+
+export const TransactionList = () => {
+  const { transactions, getTransactions } = useContext(GlobalContext);
+  const user = useSelector(state => state.authentication.user);
+
+  useEffect(() => {
+    getTransactions(user.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction._id} transaction={transaction} />))}
+      </ul>
+    </>
+  )
+}
